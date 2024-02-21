f7957b879a1d024e2fc7a5ca9d94969045112877
Fernndo <janyo1202@gmail.com>
2024-02-21 16:58:12 -0500

    add javascript ahorcado

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const array_letters = ['a','b','c','d','e','f','g','h','i','j','k','l', 'ñ','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 // creamos el array de palabras para adivinar 
 const array_words =["samantha","fernando","noa"]
-const word=array_words[0]
+
 // seleccionar el contenedor de nuestras teclas 
 const container_keys= document.getElementById("buttons_letters");
 // selecionamos el contedor dfe las palabras
@@ -10,17 +10,21 @@ const containerWords = document.querySelector(".contenedor-linea");
 const btn_start= document.getElementById("inicio")
 let image = document.getElementById("imagen-game")
 let errorCounter=1
+let buttoDisable = true
 btn_start.addEventListener("click",paintletter)
-// creamos un ciclo for para crear cada una de las letras recorriendo el array 
+let valuemin = 0
+let valuemax = array_words.length
+let word = "";
+// creamos un ciclo for para crear  cada una de las letras recorriendo el array 
 for (let index = 0; index < array_letters.length; index++) {
-    console.log("letras"+ array_letters[index])
-    // creamos la etiqueta que deceamos en este caso button
+    // creamos la etiqueta que deceamos en este caso a
     let key = document.createElement("button");
     key.classList.add("letra")
     // asignar el evento click a la etiqueta creada 
-    key.addEventListener('click', click_letras)
-    // asigno la letra a la etiqueta html 
+    key.addEventListener('click', click_letras);
+    // asigno la letra a la equite html 
     key.innerHTML=array_letters[index].toUpperCase()
+    buttoDisable == true ? key.disabled= true: key.disabled= false
     // pintamos o agragamos nuestras teclas al dom 
     container_keys.appendChild(key)
 }
@@ -29,8 +33,9 @@ function click_letras(event) {
     let button = event.target
     let letter = button.innerHTML
     let errorflag = false
+    
+    console.log("word",word)
     for (let i = 0; i < word.length; i++) {
-        
         if (letter === word[i].toUpperCase()){
             let elements_word = document.querySelectorAll(".contenedor-linea .linea")
             elements_word[i].innerHTML=letter
@@ -43,24 +48,34 @@ function click_letras(event) {
         let src=`/img/ahorcado/hm${errorCounter}.png`
         image.setAttribute("src", src);
     }
-    
 }
 function paintletter(event) {
-    
-    console.log("click aqui")
-    
-    // pinta los objetos linea en el dom de la palabra seleccionada 
+    cleanletter()
+    word=randomletter()
+    keys = document.querySelectorAll("#buttons_letters button")
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        key.disabled=false;
+    }
+    // pinta los objetos line en el dom de la palabra seleccionada 
     for (let i = 0; i < word.length; i++) {
         let letterDom =document.createElement("div");
         letterDom.classList.add("linea")
         containerWords.appendChild(letterDom)
     }
-
-
 }
 function changeSource(errorCounter){
-    console.log(errorCounter)
     if(errorCounter >= 15){
-        window.idPerdio.click()
+       window.idPerdio.click()
     }
+}
+function cleanletter(){
+    let containerletters = document.getElementById("letras_dom");
+    containerletters.innerHTML = '';
+}
+function randomletter(){
+    let  randomvalue = Math.floor(Math.random()*(valuemax-valuemin)+valuemin)
+    const word= array_words[randomvalue]
+    console.log("palabra ramdom",array_words[randomvalue])
+    return  word 
 }
